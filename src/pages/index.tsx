7fafcb1b60b7b34c2bba4b686a9ec035a0e2501d
Viral-Ahir <ahirviral01@gmail.com>
2024-10-02 18:42:53 -0400

    Added name in README

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,28 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import Profile from "./components/profile";
+import Profile2 from "./qcomps/profile_mistake";
+import MyComp from "./qcomps/firstcomp";
+import Bio from "./qcomps/bios";
+import Todos from "./qcomps/todos";
+import Profile3 from "./components/profile_props";
+import Gallery from "./qcomps/gallery_props";
+import PackingList from "./components/props_item";
+import PackingList2 from "./qcomps/props_item";
 
 export default function Home() {
   return (
-    <Profile />
-  )
+    <>
+      <Profile />
+      <Profile2 />
+      <MyComp />
+      <Bio />
+      <Todos />
+      <Profile3 />
+      <Gallery />
+      <PackingList />
+      <PackingList2 />
+    </>
+  );
 }
